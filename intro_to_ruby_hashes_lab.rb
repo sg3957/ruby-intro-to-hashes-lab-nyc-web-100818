ff39dc487a592420ca1cf190d840310c67bfb55f
Sydney Grant <sg3957@nyu.edu>
2018-09-30 02:02:31 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -61,7 +61,9 @@ def monopoly_with_fourth_tier
 	      :reading_railroad => {
 	        "mortgage_value" => "$100"
 	      }
-	      :pennsylvania_railroad => {}
+	      :pennsylvania_railroad => {
+	        "mortgage_value" =>
+	      }
 	      :b_and_o_railroad => {}
 	      :shortline => {}
 	    }
